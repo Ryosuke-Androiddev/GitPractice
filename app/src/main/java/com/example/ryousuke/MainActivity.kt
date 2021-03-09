2a14c11f5883baaa8a73c3521e8292c15a7edf0f
Air-forse1 <Timber123987456@gmail.com>
2021-03-09 13:06:47 +0900

    final commit change 1

--- app/src/main/java/com/example/ryousuke/MainActivity.kt
+++ app/src/main/java/com/example/ryousuke/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("second commit")
         println("you did branch")
-        println("change here again!!")
+        println("change here 2 times!!")
         print("Third change here!!")
     }
 }
